34a0c3f004eeef9d881c6b8bf5240fa53409b3a4
lorenzo <lorenzo.softdev@gmail.com>
2023-08-20 22:32:04 +0200

    logout func

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,13 +2,14 @@ import {
   Controller,
   Post,
   UseGuards,
-  Request,
   Get,
   Req,
   Session,
   Body,
   HttpCode,
   HttpStatus,
+  Res,
+  HttpException,
 } from '@nestjs/common';
 import { LocalAuthGuard } from './guards';
 import { Public } from 'src/utils/decorators';
@@ -17,10 +18,20 @@ import { AuthEntity } from './entities';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto';
 import { UserEntity } from 'src/users/entities';
+import { Request, Response } from 'express';
+import { SESSION_NAME } from 'src/utils/constants';
+
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Public()
+  @Get('ok')
+  @HttpCode(HttpStatus.OK)
+  isOk() {
+    return { message: 'ok' };
+  }
 
   @Public() // I think public isn't required but it's more intuitive with
   @UseGuards(LocalAuthGuard)
@@ -30,7 +41,7 @@ export class AuthController {
   @ApiOkResponse({ type: UserEntity })
   @Post('login')
   @HttpCode(HttpStatus.OK)
-  async loginLocal(@Request() req) {
+  async loginLocal(@Req() req: Request) {
     return req.user;
   }
 
@@ -40,6 +51,23 @@ export class AuthController {
   @ApiOkResponse({ type: AuthEntity })
   async loginJwt(@Body() { username, password }: LoginDto) {
     return this.authService.loginJwt(username, password);
+  }
+
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  async LogoutLocal(@Req() req: Request, @Res() res: Response) {
+    req.logout(function (err) {
+      if (err) {
+        throw new HttpException(err, HttpStatus.BAD_REQUEST);
+      }
+      req.session.destroy((err) => {
+        if (err) {
+          throw new HttpException(err, HttpStatus.BAD_REQUEST);
+        }
+        res.clearCookie(SESSION_NAME);
+        res.redirect('/api/auth/ok');
+      });
+    });
   }
 
   @Get('session')
